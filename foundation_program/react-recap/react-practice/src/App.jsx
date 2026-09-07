ad9897caa3a57595a9e3f3a1f6670cd853b7844b
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-07 23:45:17 +0600

    revising react

---
+++ foundation_program/react-recap/react-practice/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import TestComponent from './components/TestComponent';
+
+function App() {
+  return <TestComponent />;
+}
+
+export default App;
---
+++ foundation_program/react-recap/react-practice/src/components/TestComponent.jsx
@@ -0,0 +1,3 @@
+export default function TestComponent() {
+  return <h1>Test Component</h1>;
+}
