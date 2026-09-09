b84b8ec8c3d183681eb8683de07ebab7982a0cd9
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-10 02:21:05 +0600

    useState

---
+++ foundation_program/react-recap/react-practice/src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ name, age }) {
+  return (
+    <div className="outline text-black px-6 py-2 my-4 rounded-md outline-amber-300 hover:outline-amber-500">
+      <p>Name: {name}</p>
+      <p>Name: {age}</p>
+    </div>
+  );
+}
