7f33eb21d9b6ae354896d35b6c9ca8b818119430
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-14 14:35:06 +0600

    basic home screen with location picker modal

--- foundation_program/react-recap/foundation-weather-app/src/pages/Home.jsx
+++ foundation_program/react-recap/foundation-weather-app/src/pages/Home.jsx
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+import LocationPicker from '../components/LocationPicker';
+
 export default function Home() {
-  return <div>Homepage</div>;
+  const [showLocationPicker, setShowLocationPicker] = useState(false);
+  return (
+    <div className="flex flex-col gap-y-4 items-center">
+      <h1 className="text-6xl text-blue-300 text-center">
+        Foundation <span className="text-blue-700 font-bold">Weather</span> App
+      </h1>
+      <p className="text-center text-lg text-gray-500">
+        Check today's weather now...
+      </p>
+
+      <button
+        type="button"
+        className="px-3 py-1 bg-green-300 rounded-md hover:cursor-pointer hover:bg-green-500 hover:text-green-900 hover:scale-105 transition-all duration-200"
+        onClick={() => setShowLocationPicker(true)}
+      >
+        Check Weather
+      </button>
+      {showLocationPicker && (
+        <LocationPicker onClose={() => setShowLocationPicker(false)} />
+      )}
+    </div>
+  );
 }
