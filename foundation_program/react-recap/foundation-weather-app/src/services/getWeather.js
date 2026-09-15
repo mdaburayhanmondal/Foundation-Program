f6254d6d7b929b0deb9ae7ee027d52ba2d63528f
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-15 12:16:19 +0600

    function to get weather

---
+++ foundation_program/react-recap/foundation-weather-app/src/services/getWeather.js
@@ -0,0 +1,19 @@
+export const getWeather = async (place) => {
+  const { name, lat, lon } = place;
+
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,wind_speed_10m,weather_code`;
+
+  const result = await fetch(url);
+  const data = await result.json();
+  const now = data.current;
+  if (!now) {
+    throw new Error('Failed to get weather details!');
+  }
+  console.log(now);
+  return {
+    location: name,
+    temperature: Math.round(now.temperature_2m),
+    humidity: now.relative_humidity_2m,
+    wind: now.wind_speed_10m,
+  };
+};
