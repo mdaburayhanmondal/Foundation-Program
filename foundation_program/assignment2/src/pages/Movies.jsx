655ec6bfcee6d75e80f80d0b4df0541823f94ba1
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-17 15:23:32 +0600

    handled loading state

--- foundation_program/assignment2/src/pages/Movies.jsx
+++ foundation_program/assignment2/src/pages/Movies.jsx
@@ -6,17 +6,22 @@ import { searchMovies } from '../services/searchMovies';
 export default function Movies() {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(async () => {
+      setIsLoading(true);
+
       if (search.trim() === '') {
         const data = await getMovies();
+        setIsLoading(false);
         setMovies(data);
         return;
       }
 
       const data = await searchMovies(search);
       setMovies(data);
+      setIsLoading(false);
     }, 500);
 
     return () => {
@@ -34,7 +39,9 @@ export default function Movies() {
         className="mb-8 w-full rounded-lg border border-gray-300 px-4 py-3 outline-none focus:border-red-500"
       />
 
-      {movies.length === 0 ?
+      {isLoading ?
+        <p className="text-center text-4xl text-yellow-500">Loading...</p>
+      : movies.length === 0 ?
         <p className="text-center text-4xl text-red-500">No movies found.</p>
       : <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {movies.map((movie) => (
