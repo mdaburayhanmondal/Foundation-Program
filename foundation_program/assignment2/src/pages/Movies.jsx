42c86522da71c0e9166647914b09d89bd85b8059
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-17 13:55:25 +0600

    search functionality done

--- foundation_program/assignment2/src/pages/Movies.jsx
+++ foundation_program/assignment2/src/pages/Movies.jsx
@@ -1,28 +1,47 @@
 import { useEffect, useState } from 'react';
 import MovieCard from '../components/MovieCard';
 import { getMovies } from '../services/getMovies';
+import { searchMovies } from '../services/searchMovies';
 
 export default function Movies() {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    const loadMovies = async () => {
-      const data = await getMovies();
-      setMovies(data);
-    };
+    const timer = setTimeout(async () => {
+      if (search.trim() === '') {
+        const data = await getMovies();
+        setMovies(data);
+        return;
+      }
 
-    loadMovies();
-  }, []);
+      const data = await searchMovies(search);
+      setMovies(data);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search]);
 
   return (
     <section className="mx-auto w-full px-4 py-10">
-      <h1 className="mb-8 text-3xl font-bold text-center">All Movies</h1>
+      <input
+        type="text"
+        placeholder="Search movies..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="mb-8 w-full rounded-lg border border-gray-300 px-4 py-3 outline-none focus:border-red-500"
+      />
 
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+      {movies.length === 0 ?
+        <p className="text-center text-4xl text-red-500">No movies found.</p>
+      : <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      }
     </section>
   );
 }
