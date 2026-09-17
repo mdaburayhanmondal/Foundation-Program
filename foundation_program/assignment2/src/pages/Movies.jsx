e9cea7dde1c7bb3760d3243e9912623e74877d58
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-09-17 13:28:49 +0600

    basic movies list

--- foundation_program/assignment2/src/pages/Movies.jsx
+++ foundation_program/assignment2/src/pages/Movies.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from 'react';
+import MovieCard from '../components/MovieCard';
+import { getMovies } from '../services/getMovies';
+
 export default function Movies() {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      const data = await getMovies();
+      setMovies(data);
+    };
+
+    loadMovies();
+  }, []);
+
+  return (
+    <section className="mx-auto w-full px-4 py-10">
+      <h1 className="mb-8 text-3xl font-bold text-center">All Movies</h1>
+
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  );
 }
